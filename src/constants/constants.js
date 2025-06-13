e49d4593994fb2312735a81ee2ed12ca373fbe33
Vladimir Trubkin <vova.trubkin.03@mail.ru>
2025-06-13 18:36:26 +0500

    правки

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,4 +3,4 @@ export const MAX_BOUNDARY = 250;
 export const DEFAULT_FOV_ANGLE = 72;
 export const DEFAULT_FORMAT = '1:1';
 export const API_BASE_URL = 'http://localhost:5141/api';
-export const MINIO_URL = 'http://localhost:9000';
+export const MINIO_URL = 'http://83.222.16.222::9000';
