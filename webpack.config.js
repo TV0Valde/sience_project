31e4dcb1ee15d7b88fe8d3104f425b3da04de65b
Vladimir Trubkin <vova.trubkin.03@mail.ru>
2025-06-14 16:49:48 +0500

    правки

--- webpack.config.js
+++ webpack.config.js
@@ -9,17 +9,17 @@ module.exports ={
         publicPath: '/dist/' // Указываем publicPath
     },
 
-    devServer: {
- host: '0.0.0.0',
+   devServer: {
+  host: '0.0.0.0',
   port: 8080,
-allowedHosts: ['digitalshadows.run.place'],
+  allowedHosts: ['.digitalshadows.run.place', 'localhost'], // точка в начале = все поддомены
   hot: true,
-  historyApiFallback: true,        
-        open: {
-            target: 'index.html' // указываем цель открытия
-        },
-        static: {
-            directory: path.join(__dirname, 'dist') // указываем папку dist как статическую директорию
-        }
-    }
+  historyApiFallback: true,
+  open: {
+    target: 'index.html'
+  },
+  static: {
+    directory: path.join(__dirname, 'dist')
+  }
+}
 };
